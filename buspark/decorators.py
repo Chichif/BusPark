e732211a552eb9178b78c4fe78cd6c5aaca9db9c
Chichif <dmitriy.larikov8@gmail.com>
2023-05-14 00:39:39 +0200

    виправлення

--- buspark/decorators.py
+++ buspark/decorators.py
@@ -2,6 +2,6 @@ def are_there_buses(func):
     def wrapper(self, *args, **kwargs):
         if not self.buses:
             return self.show_menu('Жодного автобусу не існує, пропонуємо створити хоч якийсь!')
-        return func(*args, **kwargs)
+        return func(self, *args, **kwargs)
     return wrapper
 
