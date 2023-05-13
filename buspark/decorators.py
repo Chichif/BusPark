ba4fa944430cb2f279a97d782e3f47748b064964
Chichif <dmitriy.larikov8@gmail.com>
2023-05-14 01:38:00 +0200

    створено: декоратор 'are_there_routes' для перевірки існування хоч одного  маршруту

--- buspark/decorators.py
+++ buspark/decorators.py
@@ -5,3 +5,11 @@ def are_there_buses(func):
         return func(self, *args, **kwargs)
     return wrapper
 
+
+def are_there_routes(func):
+    def wrapper(self, *args, **kwargs):
+        if not self.routes:
+            return self.show_menu('Жодного маршруту не існує, пропонуємо створити хоч якийсь!')
+        return func(self, *args, **kwargs)
+    return wrapper
+
