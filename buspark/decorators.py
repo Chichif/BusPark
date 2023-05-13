4a654f8b627f6e81df24471907f8c7bc8d13eaab
Chichif <dmitriy.larikov8@gmail.com>
2023-05-13 23:58:29 +0200

    створено: декомпозований в окремий модуль декоратор 'is_there_buses'

---
+++ buspark/decorators.py
@@ -0,0 +1,7 @@
+def are_there_buses(func):
+    def wrapper(self, *args, **kwargs):
+        if not self.buses:
+            return self.show_menu('Жодного автобусу не існує, пропонуємо створити хоч якийсь!')
+        return func(*args, **kwargs)
+    return wrapper
+
