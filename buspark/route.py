9363234c8f0986077ade010294219f9517e41e10
Chichif <dmitriy.larikov8@gmail.com>
2023-05-14 01:44:12 +0200

    оновлено: поведінку методів 'remove_bus' та 'add_bus'

--- buspark/route.py
+++ buspark/route.py
@@ -2,13 +2,14 @@ class Route:
     def __init__(self, start_point: str, end_point: str):
         self.start_point: str = start_point
         self.end_point: str = end_point
+        self.bus_list = []
 
     def add_bus(self, bus):
-        bus.status = 'На маршруті'
         bus.route = self
         self.bus_list.append(bus)
 
     def remove_bus(self, bus):
+        bus.route = None
         self.bus_list.remove(bus)
 
     def get_buses_on_route(self):
