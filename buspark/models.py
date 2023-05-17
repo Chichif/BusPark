215dea8d8c31c59eb6a45bf382520cb3ff01eb03
Chichif <dmitriy.larikov8@gmail.com>
2023-05-17 17:09:20 +0200

    створено: pydantic моделі для ліпшої типізації, а також декомпозовано у окремий модуль

---
+++ buspark/models.py
@@ -0,0 +1,79 @@
+from pydantic import BaseModel
+from enum import Enum
+from datetime import datetime
+
+
+class City(BaseModel):
+    title: str
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Route(BaseModel):
+    start_point: City
+    end_point: City
+
+    
+    def __str__(self) -> str:
+        return f'маршрут {self.start_point} - {self.end_point}'
+    
+
+class BusStatusEnum(str, Enum):
+    ON_THE_ROAD = 'У дорозі'
+    IN_THE_PARKING = 'У парку'
+
+
+class Bus(BaseModel):
+    number: str
+    driver_name: str
+    route: Route = None
+    status: BusStatusEnum = BusStatusEnum.IN_THE_PARKING
+
+    def __str__(self) -> str:
+        return f'автобус {self.number}'
+
+
+class Departure(BaseModel): 
+    bus: Bus
+    route: Route
+    departure_time: datetime = None
+    arrival_time: datetime | None = None
+
+
+    def start_travel(self):
+        self.departure_time = datetime.now()
+        self.bus.status = BusStatusEnum.ON_THE_ROAD
+
+
+    def finish_travel(self):
+        self.arrival_time = datetime.now()
+        self.bus.status = BusStatusEnum.IN_THE_PARKING
+
+
+    @property
+    def travel_time(self) -> str:
+        if not self.arrival_time:
+            time_difference = datetime.now() - self.departure_time
+        else:
+            time_difference = self.arrival_time - self.departure_time
+        hours = time_difference.total_seconds() // 3600
+        minutes = (time_difference.total_seconds() // 60) % 60
+        seconds = time_difference.total_seconds() % 60
+
+        time_str = f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
+        return time_str
+
+
+class Park(BaseModel):
+    bus_list: list[Bus] = []
+
+    def add_bus(self, bus: Bus):
+        self.bus_list.append(bus)
+    
+    def remove_bus(self, bus: Bus):
+        self.bus_list.remove(bus)
+
+    @property
+    def parked_buses(self):
+        return self.bus_list
