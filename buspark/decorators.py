50caa7faa761e28be839340e660e015d51debeac
Chichif <dmitriy.larikov8@gmail.com>
2023-05-17 17:28:20 +0200

    Рефакторинг
    створено: декоратор 'are_there_departed_buses', додано: коментування

--- buspark/decorators.py
+++ buspark/decorators.py
@@ -1,5 +1,13 @@
+from functools import wraps
+
 def are_there_buses(func):
+    @wraps
     def wrapper(self, *args, **kwargs):
+        """
+        Декоратор, який перевіряє наявність автобусів перед викликом функції.
+
+        Якщо жодного автобусу немає, виводить повідомлення про створення нового автобусу.
+        """
         if not self.bus_list:
             return self.show_menu('Жодного автобусу не існує, пропонуємо створити хоч якийсь!')
         return func(self, *args, **kwargs)
@@ -7,9 +15,28 @@ def are_there_buses(func):
 
 
 def are_there_routes(func):
+    @wraps
     def wrapper(self, *args, **kwargs):
+        """
+        Декоратор, який перевіряє наявність маршрутів перед викликом функції.
+
+        Якщо жодного маршруту немає, виводить повідомлення про створення нового маршруту.
+        """
         if not self.route_list:
             return self.show_menu('Жодного маршруту не існує, пропонуємо створити хоч якийсь!')
         return func(self, *args, **kwargs)
     return wrapper
 
+
+def are_there_departed_buses(func):
+    @wraps
+    def wrapper(self, *args, **kwargs):
+        """
+        Декоратор, який перевіряє наявність відправлених автобусів перед викликом функції.
+
+        Якщо жодного активного відправлення немає, виводить повідомлення про відсутність автобусів у дорозі.
+        """
+        if not self.active_departures:
+            return self.show_menu("[!] Автобуси у дорозі відсутні!")
+        return func(self, *args, **kwargs)
+    return wrapper
